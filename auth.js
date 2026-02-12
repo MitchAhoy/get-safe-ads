2433d1cdd43f192b7da11817b62352249d5fba2f
mitch <mitch_mitch_w@hotmail.com>
2026-02-12 19:46:02 +1100

    add google auth

--- auth.js
+++ auth.js
@@ -11,7 +11,10 @@ const config = {
       from: "noreply@mail.getsafeads.com",
       name: "email",
     }),
-    Google({}),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
   ],
   adapter: MongoDBAdapter(clientPromise),
 };
