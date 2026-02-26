333ab5febd6cabc138d9b07d6b23fc8d956c183c
mitch <mitch_mitch_w@hotmail.com>
2026-02-27 09:17:31 +1100

    createUser.js

--- auth.js
+++ auth.js
@@ -3,6 +3,7 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/lib/mongo";
 import Resend from "next-auth/providers/resend";
 import Google from "next-auth/providers/google";
+import createUser from "./lib/createUser";
 
 const config = {
   providers: [
@@ -17,6 +18,12 @@ const config = {
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
+  events: {
+    async createUser({ user }) {
+      console.log("createUser auth event fired " + user.id);
+      createUser(user.id);
+    },
+  },
 };
 
 export const { handlers, signIn, signOut, auth } = NextAuth(config);
